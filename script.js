71e1d68235bbaad7555d576fbacde705bf64ec08
sahanasanthosh17-source <sahanasanthosh17@gmail.com>
2026-03-25 01:47:16 +0530

    added files

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollToContact() {
+    document.getElementById("contact").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function sendMessage() {
+    alert("Thank you! I will contact you soon 😊");
+}
